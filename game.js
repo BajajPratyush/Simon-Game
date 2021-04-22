f16efca6b9fd6d8e86416d06c351e4c2d092c4e6
BajajPratyush <pratyush.bajaj123@gmail.com>
2021-04-22 21:21:45 +0530

    commit on 'main'

--- game.js
+++ game.js
@@ -21,39 +21,39 @@ $(".btn").click(function() {
 });
 
 function checkAnswer(currentLevel) {
-
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-        console.log("success");
-        if (userClickedPattern.length === gamePattern.length) {
-            setTimeout(function() {
-                nextSequence();
-            }, 1000);
-        }
-    } else {
-        setTimeout(function() {
-            var audio = new Audio("sounds/wrong.mp3");
-            audio.play();
-        }, 100);
-        var audio = new Audio("sounds/wrong.mp3");
-        $("body").addClass("game-over");
-        setTimeout(function() {
-            $("body").removeClass("game-over");
-        }, 200);
-        $("#level-title").text("Game Over, Press Any Key to Restart");
-        gamePattern = [];
-        userClickedPattern = [];
-        flag = false;
-        level = 0;
-        $(document).keypress(function() {
-            if (flag === false) {
-                $("#level-title").text("Level " + level);
-                nextSequence();
-                flag = true;
+    if (flag) {
+        if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+            console.log("success");
+            if (userClickedPattern.length === gamePattern.length) {
+                setTimeout(function() {
+                    nextSequence();
+                }, 1000);
             }
-        });
+        } else {
+            setTimeout(function() {
+                var audio = new Audio("sounds/wrong.mp3");
+                audio.play();
+            }, 100);
+            var audio = new Audio("sounds/wrong.mp3");
+            $("body").addClass("game-over");
+            setTimeout(function() {
+                $("body").removeClass("game-over");
+            }, 200);
+            $("#level-title").text("Game Over, Press Any Key to Restart");
+            gamePattern = [];
+            userClickedPattern = [];
+            flag = false;
+            level = 0;
+            $(document).keypress(function() {
+                if (flag === false) {
+                    $("#level-title").text("Level " + level);
+                    nextSequence();
+                    flag = true;
+                }
+            });
+        }
     }
 }
-
 
 function nextSequence() {
     userClickedPattern = [];
